30a1e4547564f5caff752d1957f6470918b9d9eb
Markus23D <maan0007@stud.kea.dk>
2023-09-25 13:07:12 +0200

    kavet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("wassup");
+        System.out.println("wassup okay");
+        System.out.println("Ghost");
     }
 }
